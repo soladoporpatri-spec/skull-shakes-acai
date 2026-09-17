9c966f505f5b6bdb0f1e121dff067627f36ff1e7
Patricio Paulo <soladoporpatri@gmail.com>
2026-09-17 18:13:04 -0300

    fix(admin): redirect root to dashboard

--- skull-shakes-admin/next.config.js
+++ skull-shakes-admin/next.config.js
@@ -1,33 +1,19 @@
-/** @type {import('next').NextConfig} */
-
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-
   images: {
-
     remotePatterns: [
-
-      {
-
-        protocol: 'https',
-
-        hostname: '**',
-
-      },
-
-      {
-
-        protocol: 'http',
-
-        hostname: '**',
-
-      },
-
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
     ],
-
   },
-
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ];
+  },
 }
-
-
-
 module.exports = nextConfig
